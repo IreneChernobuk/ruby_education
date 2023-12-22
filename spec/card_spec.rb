78b75abee8a9071da39c8357d521fed2b97f40fd
IrinaChernobuk <irinka_buc@mail.ru>
2023-12-22 17:46:11 +0300

    rspec education part 1

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,5 +1,6 @@
 class Card
   attr_reader :rank, :suit
+
   def initialize(rank, suit)
     @rank = rank
     @suit = suit
@@ -7,16 +8,20 @@ class Card
 end
 
 RSpec.describe Card do
-  before do
-    puts "hey, I'll output before EACH example"
-    @card = Card.new('Ace', 'Spades')
-  end
+  let(:card) { Card.new('Ace', 'Spades') }
+  # let(:x) { 1 + 1 }
+  # let(:y) { x + 10 }
 
-  it 'has a rank' do
-    expect(@card.rank).to eq('Ace')
+  it 'has a rank and that rank can change' do
+    expect(card.rank).to eq('Ace')
   end
 
   it 'has a suit' do
-    expect(@card.suit).to eq('Spades')
+    expect(card.suit).to eq('Spades')
+  end
+
+  it 'has a custom error message' do
+    comparison = 'Spades'
+    expect(card.suit).to eq(comparison), "Hey, I expected #{comparison} but i got #{card.suit} instead"
   end
 end
